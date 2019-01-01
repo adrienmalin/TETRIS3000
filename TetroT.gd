80fb1421bdb043b13568f6cc41bc76d7f2e8c8a9
adrienmalin <41926238+adrienmalin@users.noreply.github.com>
2019-01-01 04:09:52 +0100

    clean up mess

--- TetroT.gd
+++
@@ -1,6 +0,0 @@
-extends Spatial
-
-var minos
-
-func _ready():
-	minos = [$Mino, $Mino2, $Mino3, $Mino4]
--- Tetrominos/TetroT.gd
+++ Tetrominos/TetroT.gd
@@ -1,26 +1,6 @@
-extends "Tetromino.gd"
-    
-const T_SLOT = [
-	Vector3(-1, 1, 0),
-	Vector3(1, 1, 0),
-	Vector3(1, -1, 0),
-	Vector3(-1, -1, 0)
-]
+extends Spatial
 
-func rotate(direction):
-	if .rotate(direction):
-		detect_t_spin()
-		return true
-	return false
-	
-func detect_t_spin():
-	var center = to_global(minoes[0].translation)
-	var a = not get_parent().is_free_cell(center + T_SLOT[orientation])
-	var b = not get_parent().is_free_cell(center + T_SLOT[(1+orientation)%4])
-	var c = not get_parent().is_free_cell(center + T_SLOT[(2+orientation)%4])
-	var d = not get_parent().is_free_cell(center + T_SLOT[(3+orientation)%4])
-	if a and b and (c or d):
-		t_spin = T_SPIN
-	elif c and d and (a or b):
-		if t_spin != T_SPIN:
-			t_spin = MINI_T_SPIN
+var minos
+
+func _ready():
+	minos = [$Mino, $Mino2, $Mino3, $Mino4]
