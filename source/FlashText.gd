bdba103417e8df67b773b7cc4d49170453e4ef54
adrienmalin <41926238+adrienmalin@users.noreply.github.com>
2019-01-27 14:18:42 +0100

    fix flash text (again)

--- source/FlashText.gd
+++ source/FlashText.gd
@@ -10,7 +10,5 @@ func print(text):
 	$AnimationPlayer.play("Flash")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	texts = PoolStringArray()
-
-func _on_Stats_flash_text(text):
-	self.print(text)
+	texts.resize(0)
+	
